cfdb79e6435568cdd2e72b7f3dec18ccaa5669bc
marxin <marxin.liska@gmail.com>
2013-06-09 11:49:25 +0200

    readpage_graph introduced.

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ List of scripts:
 + gcc-reorder-runner.sh - wrapper running batch of gcc binaries (function reorder specific)
 + readelf_sections - wrapper that will print all sections presented in ELF binary, display modes: none|latex|padded
 + readelf_relocs - prints for each relocation type number of relocations
-+ ldd_informer - shows all dependencies of executable including size of all shared libraries 
++ ldd_informer - shows all dependencies of executable including size of all shared libraries
++ readpage_graph - creates a graph from stap dump and binary file; all important ELF sections are highlighted
---
+++ readpage_graph.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import matplotlib.patches
+import matplotlib.pyplot as plt
+import matplotlib.collections as collections
+from matplotlib.ticker import FuncFormatter
+from itertools import *
+import numpy as np
+
+alpha = 0.25
+
+if len(sys.argv) < 3:
+  print('usage: readpage_graph.py [data] [elf_executable] <graph_file>')
+  exit(-1)
+
+graph_sections = (('.text', 'b'), ('.rel.dyn', 'r'), ('.rela.dyn', 'r'), ('.data.rel.ro', 'y'), ('.eh_frame', 'm'), ('.rodata', 'c'), ('.dynstr', '#ffa500'), ('.symtab', '#666f00'), ('.strtab', '#006f66'), ('.init_array', '#000000'))
+
+def parse_section_name(line):
+  s = line.find(']') + 2
+  e = line.find(' ', s)
+  return line[s:e]
+
+def parse_size(line):
+  return int(line.split(' ')[0], 16)
+
+def parse_readelf(target):
+  f = os.popen('readelf -S ' + target)
+
+  lines = f.readlines()[5:-4]
+
+  i = 0
+  total = 0
+  sections = []
+
+  while i < len(lines):
+    line = lines[i].strip()
+    name = parse_section_name(line)
+    offset = int(line.split(' ')[-1], 16)
+
+    i += 1
+
+    line = lines[i].strip()
+    size = parse_size(line)
+    sections.append((name, offset, size))
+    total += size
+
+    i += 1
+
+  return sections
+
+f = open(sys.argv[1], 'r')
+
+k = 2
+fig = plt.figure(figsize = (k * 18, k * 10))
+ax = fig.add_subplot(111)
+xs = []
+ys = []
+
+start = 0
+for line in f:
+  columns = line.split(' ')
+  if start == 0:
+    start = int(columns[0])
+
+  xs.append(int(columns[0]) - start)
+  ys.append(int(columns[2].strip()))
+
+sections = filter(lambda x: x[0] in map(lambda x: x[0], graph_sections),  parse_readelf(sys.argv[2]))
+
+maxx = max(xs)
+legends = [[], []]
+
+for s in sections:
+  item = next(ifilter(lambda x: x[0] == s[0], graph_sections), None)
+  c = collections.BrokenBarHCollection([(0, maxx)], (s[1], s[2]), facecolor = item[1], edgecolor = 'white', alpha = alpha)
+  ax.add_collection(c)
+  legends[0].insert(0, matplotlib.patches.Rectangle((0, 0), 1, 1, fc = item[1], alpha = alpha))
+  legends[1].insert(0, s[0])
+
+print(ys)
+print(xs)
+
+ax.plot(xs, ys, color='silver', marker='o', markerfacecolor='black', mew = 0, markersize=1)
+ax.set_xlim(0, maxx)
+ax.legend(legends[0], legends[1])
+
+ax.yaxis.set_major_formatter(FuncFormatter(lambda x, pos: ('%d') % (x / (1024 * 1024))))
+ax.set_ylabel('Offset (MB)')
+
+ax.xaxis.set_major_formatter(FuncFormatter(lambda y, pos: ('%d') % (y / 1000)))
+ax.set_xlabel('Time (ms)')
+
+if len(sys.argv) >= 4:
+  fig.savefig(sys.argv[3], bbox_inches = 'tight')
+else:
+  fig.show()
