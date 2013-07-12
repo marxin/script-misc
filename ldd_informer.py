2a98cc5b2644a494ca3ce8c460194da1a315ba96
marxin <marxin.liska@gmail.com>
2013-07-12 22:36:31 +0200

    ldd_informer is cabable of binary copying.

--- ldd_informer.py
+++ ldd_informer.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 import os
 import sys
+import shutil
 
 def sizeof_fmt(num):
   for x in ['B','KB','MB','GB','TB']:
@@ -11,8 +12,8 @@ def sizeof_fmt(num):
       return "%3.1f %s" % (num, x)
     num /= 1024.0
 
-if len(sys.argv) != 2:
-  print('usage: ldd_informer <executable>')
+if len(sys.argv) < 2:
+  print('usage: ldd_informer <executable> [destination_copy_folder]')
   exit(-1)
 
 target = sys.argv[1]
@@ -24,6 +25,7 @@ binsize = os.path.getsize(target)
 print('%-32s%10s' % (target_base, str(sizeof_fmt(binsize))))
 
 libraries = []
+locations = [target]
 total = 0
 
 for line in f.readlines():
@@ -32,6 +34,7 @@ for line in f.readlines():
  
   if len(tokens) == 4:
     path = tokens[2]
+    locations.append(path)
     size = os.path.getsize(path)
     total += size
     libraries.append([tokens[0], size])
@@ -46,3 +49,14 @@ print('Libraries: %31s' % sizeof_fmt(total))
 print('Number of libs: %26s' % len(libraries))
 print('Average library size: %20s' % (sizeof_fmt(total / len(libraries))))
 print('Grand total: %29s' % sizeof_fmt(total + binsize))
+
+# libraries copy process
+if len(sys.argv) == 3:
+  target_folder = sys.argv[2]
+
+  if not os.path.exists(target_folder):
+    os.mkdir(target_folder)
+
+  for library in locations:
+    print('Copying: %s -> %s' % (library, target_folder))
+    shutil.copy(library, target_folder)
