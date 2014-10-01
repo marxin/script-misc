5d6615f4e67b19802e02b964af4f7e9788b36722
mliska <mliska@suse.cz>
2014-10-01 13:30:58 +0200

    Binary size added for parser.

--- spec_new_parser.py
+++ spec_new_parser.py
@@ -13,7 +13,7 @@ def isfloat(value):
     return False
 
 def strfloat(value, rounding_digits = 2):
-  return '%.2f' % round(value, rounding_digits)
+  return '%.2f %%' % round(value, rounding_digits)
 
 ### MAIN ###
 if len(sys.argv) < 2:
@@ -28,9 +28,11 @@ profiles_file = os.path.join(profile_folder, 'profiles.txt')
 profiles = [x.strip() for x in open(profiles_file).readlines()]
 
 d = {}
+s = {}
 
 for profile in profiles:
   d[profile] = {}
+  s[profile] = {}
   for f in os.listdir(profile_folder):
     if f.startswith(profile + '_') and f.endswith('.log'):
       test = f[len(profile) + 1:-4] 
@@ -42,17 +44,23 @@ for profile in profiles:
       if len(times) > 0:
         avg = sum(times) / len(times)
         d[profile][test] = avg
+    if f == profile + '-size.csv':
+      sizes = [x.split(':') for x in open(os.path.join(profile_folder, f)).readlines()]
+      for size in sizes:
+	s[profile][size[0].split('.')[-1]] = int(size[1])
 
 first = d.keys()[0]
 tests = d[first].keys()
 
 p = {}
+p2 = {}
 
 for (profile, tests) in d.iteritems():
   p[profile] = {}
+  p2[profile] = {}
   for test in tests.keys():
-    p[profile][test] = 100 * d[first][test] / d[profile][test]
-  values = p[profile].values()
+    p[profile][test] = 100 * d[profile][test] / d[first][test]
+    p2[profile][test] = 100.0 * s[profile][test] / s[first][test]
 
 print(':', end = '')
 print(':'.join(tests), end = '')
@@ -60,6 +68,12 @@ print(':AVG')
 
 for (profile, tests) in p.iteritems():
   print(profile, end = ':')
+  print(':'.join(map(lambda x: strfloat(x), tests.values())), end = '')
+  avg = sum(tests.values()) / len(tests.values())
+  print(':' + strfloat(avg))
+
+for (profile, tests) in p2.iteritems():
+  print(profile + '_SIZE', end = ':')
   print(':'.join(map(lambda x: strfloat(x), tests.values())), end = '')
   avg = sum(tests.values()) / len(tests.values())
   print(':' + strfloat(avg))
