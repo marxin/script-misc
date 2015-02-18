7a96fb91f84cb0678185b99dd15956f02b74a6fc
mliska <mliska@suse.cz>
2015-02-18 14:50:06 +0100

    strace_parse_execv: new script.
    SPEC: config file ignores MD5 subfolder comparison.

---
+++ strace_parse_execv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import fileinput
+import sys
+
+for line in fileinput.input():
+  	i = line.find('execv')
+	line = line[i:]
+  	s = line.find('[') + 1
+	e = line.find(']')
+	cmd = line[s:e]
+	cmd = cmd.replace('"', '').replace(',', '')
+	print(cmd)
