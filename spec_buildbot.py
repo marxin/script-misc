526f90c427eb11ecafbd105a09ccda9dd40d15a5
marxin <mliska@suse.cz>
2016-02-29 15:38:01 +0100

    Simplify the script.

--- spec_buildbot.py
+++ spec_buildbot.py
@@ -20,106 +20,35 @@ import tarfile
 
 ### SPECv6 class ###
 class CpuV6:
-  def get_benchmarks(self):
-    return [
-    Benchmark('600.perlbench_s', True),
-    Benchmark('602.gcc_s', True),
-    Benchmark('603.bwaves_s', False),
-    Benchmark('605.mcf_s', True),
-    Benchmark('607.cactuBSSN_s', False),
-    Benchmark('608.namd_s', False),
-    Benchmark('610.parest_s', False),
-    Benchmark('611.povray_s', False),
-    Benchmark('613.hmmer_s', True),
-    Benchmark('619.lbm_s', False),
-    Benchmark('620.omnetpp_s', True),
-    Benchmark('621.wrf_s', False),
-    Benchmark('623.xalancbmk_s', True),
-    Benchmark('625.x264_s', True),
-    Benchmark('626.blender_s', False),
-    Benchmark('627.cam4_s', False),
-    Benchmark('628.pop2_s', True),
-    Benchmark('631.deepsjeng_s', True),
-    Benchmark('632.facesim_s', False),
-    Benchmark('638.imagick_s', False),
-    Benchmark('639.bodytrack_s', False),
-    Benchmark('641.leela_s', True),
-    Benchmark('644.nab_s', False),
-    Benchmark('647.drops_s', False),
-    Benchmark('648.exchange2_s', True),
-    Benchmark('649.fotonik3d_s', False),
-    Benchmark('651.qe_s', False),
-    Benchmark('652.mdwp_s', True), 
-    Benchmark('653.johnripper_s', True),
-    Benchmark('654.roms_s', False),
-    Benchmark('656.ferret_s', False),
-    Benchmark('657.xz_s', True)]
-
   def build_config(self, configuration, profile):
     config_template_path = os.path.join(real_script_folder, 'config-template', 'config-template-v6.cfg')
-    lines = open(config_template_path, 'r').readlines()
-
-    p = 133
-
-    flags = default_flags
-    lines.insert(p, 'OPTIMIZE = ' + flags)
-
-    p = 114
-    lines.insert(p, 'makeflags = -j' + str(cpu_count()))
-
-    p = 54
-
     compilers = configuration.compilers()
-    lines.insert(p, 'FC = ' + compilers['FC'])
-    lines.insert(p, 'CXX = ' + compilers['CXX'])
-    lines.insert(p, 'CC = ' + compilers['CC'])
-    lines.insert(p, 'EXTRA_LDFLAGS = ' + compilers['LD'])
+    flags = default_flags
+    lines = [x.strip() for x in open(config_template_path, 'r').readlines()]
+
+    new_lines = []
+    for line in lines:
+        if line == 'JOBS_PLACEHOLDER':
+            new_lines.append('makeflags = -j' + str(cpu_count()))
+        elif line == 'OPTIMIZE_PLACEHOLDER':
+            new_lines.append('OPTIMIZE = ' + flags)
+        elif line == 'COMPILERS_PLACEHOLDER':
+            new_lines.append('FC = ' + compilers['FC'])
+            new_lines.append('CXX = ' + compilers['CXX'])
+            new_lines.append('CC = ' + compilers['CC'])
+            new_lines.append('EXTRA_LDFLAGS = ' + compilers['LD'])
+        else:
+            new_lines.append(line)
 
     config_name = os.path.join(config_folder, profile + '.cfg')
     f = open(config_name, 'w+')
-
-    for l in lines:
-      f.write(l.strip() + '\n')
+    f.write('\n'.join(new_lines))
 
     ts_print('generating config to: ' + config_name)
     return config_name
 
-
 ### SPEC2006 class ###
 class Cpu2006:
-  def get_benchmarks(self):
-    return [
-      Benchmark('400.perlbench', True, False),
-      Benchmark('401.bzip2', True, False),
-      Benchmark('403.gcc', True, False),
-      Benchmark('410.bwaves', False, True),
-      Benchmark('416.gamess', False, True),
-      Benchmark('429.mcf', True, False),
-      Benchmark('433.milc', False, False),
-      Benchmark('434.zeusmp', False, True),
-      Benchmark('435.gromacs', False, True),
-      Benchmark('436.cactusADM', False, True),
-      Benchmark('437.leslie3d', False, True),
-      Benchmark('444.namd', False, False),
-      Benchmark('445.gobmk', True, False),
-      Benchmark('447.dealII', False, False),
-      Benchmark('450.soplex', False, False),
-      Benchmark('453.povray', False, False),
-      Benchmark('454.calculix', False, True),
-      Benchmark('456.hmmer', True, False),
-      Benchmark('458.sjeng', True, False),
-      Benchmark('459.GemsFDTD', False, True),
-      Benchmark('462.libquantum', True, False),
-      Benchmark('464.h264ref', True, False),
-      Benchmark('465.tonto', False, True),
-      Benchmark('470.lbm', False, False),
-      Benchmark('471.omnetpp', True, False),
-      Benchmark('473.astar', True, False),
-      Benchmark('481.wrf', False, True),
-      Benchmark('482.sphinx3', False, False),
-      Benchmark('483.xalancbmk', True, False)
-    ]
-
   def generate_config(profile, configuration, extra_flags = ''):
     lines = open(config_template, 'r').readlines()
 
@@ -178,7 +107,7 @@ class Benchmark:
     self.pure_name = name[name.find('.') + 1:]
     self.is_int = is_int
 
-if len(sys.argv) != 6:
+if len(sys.argv) != 5:
   sys.exit(1)
 
 real_script_folder = os.path.dirname(os.path.realpath(__file__))
@@ -187,8 +116,7 @@ real_script_folder = os.path.dirname(os.path.realpath(__file__))
 root_path = os.path.abspath(sys.argv[1])
 dump_file = sys.argv[2]
 compiler = sys.argv[3]
-changes = b64decode(sys.argv[4])
-perf_folder = sys.argv[5]
+changes = sys.argv[4]
 profile = 'cpuv6'
 
 configuration = None
@@ -203,8 +131,8 @@ config_folder = os.path.join(root_path, 'config')
 summary_folder = os.path.join(root_path, 'summary')
 config_template = os.path.join(real_script_folder, 'config-template', 'config-template.cfg')
 
-default_flags = '-O3 -march=native -g'
-runspec_arguments = '--size=train --no-reportable --iterations=1 --tune=peak -D '
+default_flags = '-O2 -flto=10 -march=native -g'
+runspec_arguments = '--size=test --no-reportable --iterations=1 --tune=peak --no-reportable -D all'
 
 def ts_print(*args):
   print('[%s]: ' % datetime.datetime.now(), end = '')
@@ -213,13 +141,6 @@ def ts_print(*args):
     print(a)
 
   sys.stdout.flush()
-
-# parse optimize options from commit message
-kv = 'flags:'
-if kv in changes:
-  i = changes.find(kv) + len(kv) + 1
-  default_flags = changes[i:]
-  ts_print('default flags changed from commit message: ' + default_flags)
 
 d = {
     'INT': {},
@@ -230,23 +151,11 @@ d = {
 	'runspec_flags': runspec_arguments,
 	'uname': ' '.join(platform.uname()),
 	'node': platform.uname()[1],
-	'changes': changes,
 	'compiler': compiler
       }
     }
 
 os.chdir(root_path)
-
-# perf argument detection
-proc = commands.getstatusoutput('perf --version')
-perf_version = proc[1].split(' ')[-1]
-
-perf_arguments = ['--call-graph=dwarf']
-if LooseVersion(perf_version) < LooseVersion('3.0.0'):
-  perf_arguments = ['-g', '-f']
-
-# TODO
-perf_arguments = []
 
 def save_spec_log(folder, profile, benchmark, data):
   f = open(os.path.join(folder, profile + '_' + benchmark + '.log'), 'w+')
@@ -303,21 +212,19 @@ ts_print('Starting group of tests')
 
 
 v6 = CpuV6()
-benchmarks = configuration.filter_benchmarks(v6.get_benchmarks())
+# benchmarks = configuration.filter_benchmarks(v6.get_benchmarks())
 c = v6.build_config(configuration, profile)
 
-for j, b in enumerate(benchmarks):
-  locald = d['INT'] if b.is_int else d['FP']
-  locald[b.name] = {}
+cl = runspec_command('--config=' + c + ' --output-format=raw ' + runspec_arguments)
+print(cl)
+proc = commands.getstatusoutput(cl)
 
-  ts_print('Running subphase: %u/%u: %s' % (j + 1, len(benchmarks), b.name))
+print(proc[0])
+if proc[0] != 0:
+    print(proc[1])
 
-  # Real benchmark run
-  extra = ''
 
-  cl = runspec_command('--config=' + c + ' --output-format=raw ' + runspec_arguments + b.name)
-  proc = commands.getstatusoutput(cl)
-
+"""
   ts_print('Command result: %u' % proc[0])
   if proc[0] != 0:
     locald[b.name]['times'] = None
@@ -347,7 +254,6 @@ for j, b in enumerate(benchmarks):
 
     ts_print(locald)
 
-"""
     # prepare folder
     perf_folder_subdir = os.path.join(perf_folder, b.name)
     os.makedirs(perf_folder_subdir)
@@ -392,8 +298,9 @@ for j, b in enumerate(benchmarks):
         locald[b.name]['size'] = parse_binary_size(binary)
 """
 
-
+"""
 with open(dump_file, 'w') as fp:
   json.dump(d, fp, indent = 1)
 
 ts_print(json.dumps(d, indent = 1))
+"""
