6f10a0280ea5ba048ea0f61c7aff5c4b4b11799f
marxin <mliska@suse.cz>
2016-07-27 12:47:23 +0200

    Implement new command line options.

--- branch_changer.py
+++ branch_changer.py
@@ -10,29 +10,29 @@ statuses = ['UNCONFIRMED', 'ASSIGNED', 'SUSPENDED', 'NEW', 'WAITING', 'REOPENED'
 search_summary = ' Regression]'
 regex = '(.*\[)([0-9\./]*)( [rR]egression].*)'
 
-def modify_bug(api_key, id, new_summary):
+def get_bugs(api_key, query):
+    u = base_url + 'bug'
+    r = requests.get(u, params = query)
+    return r.json()['bugs']
+
+def modify_bug(api_key, id, params, doit):
     u = base_url + 'bug/' + str(id)
 
     data = {
-        'summary': new_summary,
         'ids': [id],
         'api_key': api_key }
 
+    data.update(params)
     print(data)
 
-"""
-    r = requests.put(u, json = data)
-    print(r)
-"""
+    if doit:
+        r = requests.put(u, json = data)
+        print(r)
 
-def search(api_key, remove, add, limit):
+def search(api_key, remove, add, limit, doit):
     # 1) get bug info to find 'cc'
-    u = base_url + 'bug'
-    r = requests.get(u, params = {'api_key': api_key, 'summary': search_summary, 'bug_status': statuses})
-    bugs = r.json()['bugs']
+    bugs = get_bugs(api_key, {'api_key': api_key, 'summary': search_summary, 'bug_status': statuses})
     count = 0
-    # Python3 does not have sys.maxint
-    limit = int(limit) if limit != None else 10**10
     for b in bugs:        
         summary = b['summary']
         m = re.match(regex, summary)
@@ -58,14 +58,39 @@ def search(api_key, remove, add, limit):
                 print('Changing summary for PR%d:' % b['id'])
                 print(summary)
                 print(new_summary)
-                modify_bug(api_key, b['id'], new_summary)
+                modify_bug(api_key, b['id'], {'summary': new_summary}, doit)
                 print()
+
+def replace_milestone(api_key, limit, old_milestone, new_milestone, comment, doit):
+    bugs = get_bugs(api_key, {'api_key': api_key, 'bug_status': statuses, 'target_milestone': old_milestone})
+    count = 0
+
+    for b in bugs:
+        if count >= limit:
+            return
+        count += 1
+        print('Changing target milestone for PR%d: %s' % (b['id'], new_milestone))
+        args = {'target_milestone': new_milestone}
+        if comment != None:
+            args['comment'] = {'comment': comment }
+        modify_bug(api_key, b['id'], args, doit)
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('api_key', help = 'API key')
 parser.add_argument('--remove', nargs = '?', help = 'Remove a release from summary')
 parser.add_argument('--add', nargs = '?', help = 'Add a new release to summary, e.g. 6:7 will add 7 where 6 is included')
 parser.add_argument('--limit', nargs = '?', help = 'Limit number of bugs affected by the script')
+parser.add_argument('--doit', action = 'store_true', help = 'Really modify BUGs in the bugzilla')
+parser.add_argument('--new-target-milestone', help = 'Set a new target milestone, e.g. 4.9.3:4.9.4 will set milestone to 4.9.4 for all PRs having milestone set to 4.9.3')
+parser.add_argument('--comment', help = 'Comment a PR for which we set a new target milestore')
 
 args = parser.parse_args()
-search(args.api_key, args.remove, args.add, args.limit)
+# Python3 does not have sys.maxint
+args.limit = int(args.limit) if args.limit != None else 10**10
+
+if args.remove != None or args.add != None:
+    search(args.api_key, args.remove, args.add, args.limit, args.doit)
+if args.new_target_milestone != None:
+    t = args.new_target_milestone.split(':')
+    assert len(t) == 2
+    replace_milestone(args.api_key, args.limit, t[0], t[1], args.comment, args.doit)
