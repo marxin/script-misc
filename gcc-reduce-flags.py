c0fc05bce7fcd703ee22e1c06dd8bcbf88cb77a7
marxin <mliska@suse.cz>
2017-02-17 10:51:59 +0100

    Add gcc-reduce-flags.py script.

---
+++ gcc-reduce-flags.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+import random
+import sys
+import glob
+
+assert len(sys.argv) >= 2
+
+pattern = 'internal compiler error'
+if len(sys.argv) == 3:
+    pattern = sys.argv[2]
+
+def does_ice(command):
+    r = subprocess.run(command, shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    stderr = r.stderr.decode('utf-8')
+    return pattern in stderr
+
+def do_cmd(base, flags):
+    return '%s %s' % (' '.join(base), ' '.join(flags))
+
+command_line = [x for x in sys.argv[1].split(' ') if x != '']
+
+base = [x for x in command_line if not x.startswith('-')]
+flags = set([x for x in command_line if not x in base])
+
+def reduce(base, flags):
+    while True:
+        change = False
+        for f in list(flags):
+            flags.remove(f)
+            if does_ice(do_cmd(base, flags)):
+                change = True
+                break
+            else:
+                flags.add(f)
+
+        if not change:
+            return do_cmd(base, flags)
+
+print(reduce(base, flags))
