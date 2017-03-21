c97bf9a0ecf9cb989694132455b79710f05da356
marxin <mliska@suse.cz>
2017-03-21 23:20:32 +0100

    Add new script.

---
+++ gcc-extract-backport-patches.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import subprocess
+
+from git import Repo
+from itertools import *
+
+git_location = '/home/marxin/BIG/Programming/gcc/'
+os.chdir(git_location)
+repo = Repo(git_location)
+
+if len(sys.argv) != 2:
+    print('Usage: gcc-extract-backport-patch.py FILE')
+    exit(1)
+
+revisions = sorted([x.strip() for x in open(sys.argv[1]).readlines()])
+assert len(revisions) == len(set(revisions))
+
+commits = []
+# find commits
+log = list(repo.iter_commits('parent/master~10000..parent/master'))
+
+for revision in revisions:
+    for l in log:
+        if 'trunk@' + revision in l.message:
+            commits.append(l)
+            continue
+
+assert len(revisions) == len(commits)
+
+patches = []
+
+for i, c in enumerate(commits):
+    r = subprocess.run('git format-patch -1 %s' % c.hexsha, stdout = subprocess.PIPE, shell = True)
+    assert r.returncode == 0
+    f = r.stdout.decode('utf-8').strip()
+
+    # modify ChangeLog entries to patch body
+    lines = [x.rstrip() for x in open(f).readlines()]
+    os.remove(f)
+
+    in_changelog = False
+
+    changelog_lines = []
+    patch_lines = []
+
+    # skip beginning
+    diff_string = 'diff --git'
+    header = list(takewhile(lambda x: not x.startswith(diff_string) and not x.startswith('Subject:'), lines))    
+    lines = lines[len(header):]
+    lines = list(dropwhile(lambda x: not x.startswith(diff_string), lines))
+    while len(lines) != 0:
+        first_line = lines[0]
+        lines = lines[1:]
+        f = first_line.split(' ')
+        assert len(f) == 4
+        if f[-1].endswith('ChangeLog'):
+            changelog_lines.append(f[-1][2:] + ':')
+            changelog_lines.append('')
+            chunk = list(takewhile(lambda x: not x.startswith(diff_string), lines))
+            diff = [x[1:] for x in chunk if x.startswith('+') and not x.startswith('+++')]
+            diff = list(reversed(list(dropwhile(lambda x: x == '', reversed(diff)))))
+            if diff[-1].startswith('20'):
+                diff = [diff[-1]] + diff[:-1]
+
+            for d in diff[1:]:
+                assert not d.startswith('20')
+            changelog_lines.append('\n'.join(diff) + '\n')            
+        else:
+            chunk = list(takewhile(lambda x: not x.startswith(diff_string), lines))
+            patch_lines.append(first_line)
+            patch_lines += chunk
+
+        lines = lines[len(chunk):]
+
+    path = '{0:04d}-patch-'.format(i + 1) + revisions[i] + '.patch'
+    with open(path, 'w') as w:
+        w.write('\n'.join(header) + '\n')
+        w.write('Subject: Backport r' + revisions[i] + '\n\n')
+        w.write('\n'.join(changelog_lines) + '\n')
+        w.write('---\n')
+        w.write('\n'.join(patch_lines))
+
+    print('Generating %d/%d: %s' % (i + 1, len(revisions), path))
