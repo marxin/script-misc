16e1dd8daed08ae2f39e18568e54899608548b21
marxin <mliska@suse.cz>
2018-02-26 14:36:42 +0100

    Update script.

--- obs-diagnose-failures.py
+++ obs-diagnose-failures.py
@@ -10,21 +10,48 @@ parser.add_argument('location', help = 'Folder with logs')
 parser.add_argument('--verbose', help = 'Verbose', action='store_true')
 args = parser.parse_args()
 
-def is_interesting(line):
+def is_segfault(line):
     if 'Segmentation fault' in line or 'internal compiler error' in line:
         return True
+    return False
 
+def is_werror(line):
+    if 'error:' in line and not 'Bad exit status from' in line and '-Werror=' in line:
+        return True
+    return False
+
+def is_error(line):
     if 'error:' in line and not 'Bad exit status from' in line:
         return True
-    
     return False
+
+def find_diagnostics(lines):
+    for l in lines:
+        for d in [('segfault', is_segfault), ('Werror', is_werror), ('error', is_error)]:
+            if d[1](l):
+                return (d[0], l)
+
+    return ('unknown error', None)
+
+d = {}
 
 for root, dirs, files in os.walk(args.location):
     for f in files:
         lines = [x.strip() for x in open(os.path.join(root, f)).readlines()]
-        interesting = [l for l in lines if is_interesting(l)]
+        r = find_diagnostics(lines)
+        if not r[0] in d:
+            d[r[0]] = []
 
-        print('%s: %d' % (f, len(interesting)))
-        if (args.verbose):
-            for i in interesting:
-                print('    ' + i)
+        d[r[0]].append((f, r[1]))
+
+for (k,v) in d.items():
+    print('%16s: %5d' % (k, len(v)))
+
+if args.verbose:
+    for (k,v) in d.items():
+        print('=== %s ===' % k)
+        for p in v:
+            if p[1] != None:
+                print('%s:%s' % (p[0], p[1]))
+            else:
+                print(p[0])
