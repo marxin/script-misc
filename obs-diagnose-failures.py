7cc024b85e99cce95083f5b28c9549ed3370b20f
marxin <mliska@suse.cz>
2018-03-09 11:10:02 +0100

    Enhance scripts.

--- obs-diagnose-failures.py
+++ obs-diagnose-failures.py
@@ -8,6 +8,7 @@ import os
 parser = argparse.ArgumentParser(description = 'Analyze OBS log files')
 parser.add_argument('location', help = 'Folder with logs')
 parser.add_argument('--verbose', help = 'Verbose', action='store_true')
+parser.add_argument('--curl', '-c', help = 'Generate CURL bugzilla requests', action='store_true')
 args = parser.parse_args()
 
 def is_segfault(line):
@@ -25,9 +26,14 @@ def is_error(line):
         return True
     return False
 
+def is_post_build_error(line):
+    if ' E: ' in line:
+        return True
+    return False
+
 def find_diagnostics(lines):
     for l in lines:
-        for d in [('segfault', is_segfault), ('Werror', is_werror), ('error', is_error)]:
+        for d in [('segfault', is_segfault), ('Werror', is_werror), ('error', is_error), ('post-build-check', is_post_build_error)]:
             if d[1](l):
                 return (d[0], l)
 
@@ -55,3 +61,22 @@ if args.verbose:
                 print('%s:%s' % (p[0], p[1]))
             else:
                 print(p[0])
+
+header = 'GCC 8: XXX build fails'
+
+body = """
+Build the package with GCC 8, there's error:
+
+XXX
+
+Please build the project as follows to reproduce the issue:
+osc build --alternative-project=openSUSE:Factory:Staging:Gcc7
+""".strip()
+
+if args.curl != None:
+    for (k,v) in d.items():
+        for p in v:
+            if p[1] != None:
+                print(header.replace('XXX', p[0].rstrip('.log')))
+                print(body.replace('XXX', p[1]))
+
