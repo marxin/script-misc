91f0a3aebb32472da717899af5ef28bfa76dda39
marxin <mliska@suse.cz>
2018-06-15 10:13:25 +0200

    Add ubsan.patch.

--- gcc-bisect-v2.py
+++ gcc-bisect-v2.py
@@ -36,7 +36,7 @@ log_file = '/home/marxin/Programming/script-misc/gcc-build-v2.log'
 extract_location = '/dev/shm/gcc-bisect-bin/'
 
 patches_folder = os.path.join(script_dirname, 'gcc-bisect-patches')
-patches = ['0001-Use-ucontext_t-not-struct-ucontext-in-linux-unwind.h.patch', 'gnu-inline.patch']
+patches = ['0001-Use-ucontext_t-not-struct-ucontext-in-linux-unwind.h.patch', 'gnu-inline.patch', 'ubsan.patch']
 
 parser = argparse.ArgumentParser(description='Build GCC binaries.')
 parser.add_argument('action', nargs = '?', metavar = 'action', help = 'Action', default = 'print', choices = ['print', 'build', 'bisect', 'gc'])
