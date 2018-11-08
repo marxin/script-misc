c0efe6432440f35f096c7076b9665ccc4ff2cfdc
marxin <mliska@suse.cz>
2018-11-08 12:13:49 +0100

    Add new category.

--- obs-diagnose-failures.py
+++ obs-diagnose-failures.py
@@ -8,7 +8,6 @@ import os
 parser = argparse.ArgumentParser(description = 'Analyze OBS log files')
 parser.add_argument('location', help = 'Folder with logs')
 parser.add_argument('--verbose', help = 'Verbose', action='store_true')
-parser.add_argument('--curl', '-c', help = 'Generate CURL bugzilla requests', action='store_true')
 args = parser.parse_args()
 
 def is_segfault(line):
@@ -31,8 +30,11 @@ def is_post_build_error(line):
         return True
     return False
 
+def is_test_failure(line):
+    return 'test-suite.log] Error' in line or 'test] Error' in line
+
 def find_diagnostics(lines):
-    for d in [('segfault', is_segfault), ('Werror', is_werror), ('error', is_error), ('post-build-check', is_post_build_error)]:
+    for d in [('segfault', is_segfault), ('Werror', is_werror), ('error', is_error), ('post-build-check', is_post_build_error), ('test failure', is_test_failure)]:
         for l in lines:
             if d[1](l):
                 return (d[0], l)
@@ -61,23 +63,3 @@ if args.verbose:
                 print('%s:%s' % (p[0], p[1]))
             else:
                 print(p[0])
-
-header = 'GCC 8: XXX build fails'
-
-body = """
-Build the package with GCC 8, there's error:
-
-XXX
-
-Please build the project as follows to reproduce the issue:
-osc build --alternative-project=openSUSE:Factory:Staging:Gcc7
-""".strip()
-
-if args.curl:
-    keys = sorted(d.keys())
-    for k in keys:
-        for p in d[k]:
-            if p[1] != None:
-                print(header.replace('XXX', p[0].rstrip('.log')))
-                print(body.replace('XXX', p[1]))
-
