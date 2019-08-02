813170752879937a272d8ecf2bf0c21b55ab1a14
marxin <mliska@suse.cz>
2019-08-02 18:51:13 +0200

    Add Tumbleweed update nagios check.

---
+++ tw-check.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+from datetime import datetime
+
+lines = open('/etc/os-release').readlines()
+
+version = None
+for line in lines:
+    if line.startswith('VERSION_ID="'):
+        version = line.split('"')[1]
+
+release_date = datetime.strptime(version, '%Y%m%d')
+days = (datetime.now() - release_date).days
+
+limit = int(sys.argv[1])
+if days < limit:
+    print('OK', end = '')
+else:
+    print('WARNING', end = '')
+
+print(': last Tumbleweed update before %d days' % days)
