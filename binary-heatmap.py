7fcf263dfedf32f27dbf31388e4e61828b7dc810
Martin Liska <mliska@suse.cz>
2019-12-10 21:19:13 +0100

    Parse linker map file.

--- binary-heatmap.py
+++ binary-heatmap.py
@@ -17,6 +17,7 @@ import matplotlib.pyplot as plt
 import subprocess
 from itertools import chain
 from matplotlib.lines import Line2D
+from itertools import dropwhile, takewhile
 
 class SectionPlaceholder:
     def __init__(self, line):
@@ -33,6 +34,57 @@ class SectionPlaceholder:
                     return (self.name, self.address, p.address)
         return None
 
+class MapComponent:
+    def __init__(self, name, lines):
+        self.name = name
+        self.lines = lines[1:]
+        self.addresses = []
+        self.start = None
+        self.end = None
+
+        for l in self.lines:
+            parts = [x for x in l.split(' ') if x]
+            if len(parts) >= 1 and parts[0].startswith('0x0'):
+                self.addresses.append(int(parts[0], 16))
+
+    def get_address_range(self):
+        if self.start and self.end and self.name and self.start != self.end:
+            return (self.name, self.start, self.end)
+        else:
+            return None
+
+def parse_mapfile(filename):
+    components = []
+    map_components = [
+    (' *(.text.unlikely .text.*_unlikely .text.unlikely.*)', '.text.unlikely'),
+    (' *(.text.exit .text.exit.*)', '.text.exit'),
+    (' *(.text.startup .text.startup.*)', '.text.startup'),
+    (' *(.text.hot .text.hot.*)', '.text.hot'),
+    (' *(.text .stub .text.* .gnu.linkonce.t.*)', '.text.normal'),
+    (' *(.gnu.warning)', None),
+    ('.gnu.attributes', None)
+    ]
+
+    lines = [l.rstrip() for l in open(filename)]
+
+    lines = list(dropwhile(lambda x: x != map_components[0][0], lines))
+    for i in range(0, len(map_components) - 1):
+        chunk = list(takewhile(lambda x: x != map_components[i+1][0], lines))
+        components.append(MapComponent(map_components[i][1], chunk))
+        lines = lines[len(chunk):]
+
+    components[-1].start = components[-1].addresses[0]
+    end = components[-1].start
+
+    for i in reversed(range(len(map_components) - 1)):
+        components[i].end = end
+        if len(components[i].addresses):
+            components[i].start = components[i].addresses[0]
+            end = components[i].start
+
+    return [c.get_address_range() for c in components if c.get_address_range()]
+
+
 parser = argparse.ArgumentParser(description = 'Generate heat map of perf report')
 parser.add_argument('perf_stat_file', help = 'Output of perf stat')
 parser.add_argument('output_image', help = 'Output image')
@@ -40,7 +92,9 @@ parser.add_argument('needle', help = 'Name of the binary in perf stat')
 parser.add_argument('--title', help = 'Title')
 parser.add_argument('--max-x', help = 'Maximum value on x axis', type = int)
 parser.add_argument('--max-y', help = 'Maximum value on y axis', type = int)
-parser.add_argument('--binary', help = 'Path to binary')
+parser.add_argument('--mapfile', help = 'ld mapfile')
+parser.add_argument('--pointsize', help = 'graph point size', type = float, default = 0.2)
+parser.add_argument('--pointalpha', help = 'graph point alpha', type = float, default = 0.4)
 args = parser.parse_args()
 
 values = [l.strip() for l in open(args.perf_stat_file).readlines()]
@@ -71,7 +125,7 @@ for i in range(len(x)):
 fig, (ax1, ax2) = plt.subplots(1, 2, sharey='row', gridspec_kw={'hspace': 5, 'wspace': 0.05}, figsize=(10, 5))
 fig.suptitle(args.title)
 
-ax1.scatter(x, y, s = 0.1, c='green', alpha=0.3, edgecolors='none', marker='s')
+ax1.scatter(x, y, s = args.pointsize, c='green', alpha=args.pointalpha, edgecolors='none', marker='s')
 ax1.grid(True, linewidth = 0.5, alpha = 0.3)
 
 ax2.hist(y, 300, orientation='horizontal', color='green')
@@ -86,11 +140,8 @@ if args.max_x:
 if args.max_y:
     ax1.set_ylim((0, args.max_y))
 
-if args.binary:
-    r = subprocess.check_output('nm ' + args.binary, shell = True, encoding = 'utf8')
-    symbols = [SectionPlaceholder(l) for l in r.split('\n') if '__text_' in l]
-    ranges = [s.get_range(symbols) for s in symbols]
-    ranges = sorted([r for r in ranges if r], key = lambda x: x[0])
+if args.mapfile:
+    ranges = parse_mapfile(args.mapfile)
 
     colors = 'brcmyk'
     custom_lines = []
@@ -100,8 +151,8 @@ if args.binary:
         samples = len([a for a in y if r[1] <= a and a <= r[2]])
         fraction = (100.0 * samples / len(x))
         size = 1.0 * (r[2] - r[1]) / (1024**2)
-        custom_lines.append(Line2D([0], [0], color=colors[i], alpha=alpha, lw=4, label= '.text.' + r[0] + ' (size: %.2f MB; samples: %.2f%%)' % (size, fraction)))
+        custom_lines.append(Line2D([0], [0], color=colors[i], alpha=alpha, lw=4, label= r[0] + ' (size: %.2f MB; samples: %.2f%%)' % (size, fraction)))
         ax1.axhspan(r[1], r[2], facecolor=colors[i], alpha=alpha)
-    fig.legend(handles=custom_lines, loc = 'upper left', prop={'size': 6})
+    fig.legend(handles=list(reversed(custom_lines)), loc = 'upper left', prop={'size': 6})
 
 plt.savefig(args.output_image, dpi = 800)
