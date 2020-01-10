86c01b07c31b589f911314bfedf974e15adfb400
Martin Liska <mliska@suse.cz>
2020-01-10 10:54:15 +0100

    Update mapping script.

--- map-git-revisions.py
+++ map-git-revisions.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import os
+
 # tags changes from
 # gcc-4_7_1-release
 # to
@@ -10,14 +12,17 @@
 # to
 # origin/releases/gcc-9
 
+needed_branches = ['4.8', '4.9', '5', '6', '7', '8', '9']
+
 from git import Repo
 
-def parse_git(location, revision, old):
-    repo = Repo(location)
-    gittosvn = {}
-    svntogit = {}
+def get_revisions(repo, revision, old, seen, gittosvn, svntogit):
+    print('... doing: %s (%d)' % (revision, old))
     for revision in repo.iter_commits(revision):
         svn = None
+        if revision.hexsha in seen:
+            continue
+        seen.add(revision.hexsha)
         for l in reversed(revision.message.split('\n')):
             if old:
                 if 'git-svn-id' in l:
@@ -34,11 +39,30 @@ def parse_git(location, revision, old):
         assert svn
         gittosvn[revision.hexsha] = svn
         svntogit[svn] = revision.hexsha
+
+def parse_git(location, revision, old):
+    repo = Repo(location)
+    gittosvn = {}
+    svntogit = {}
+    seen = set()
+    get_revisions(repo, revision, old, seen, gittosvn, svntogit)
+    for br in needed_branches:
+        bname = ('parent/gcc-%s-branch' % br).replace('.', '_') if old else 'origin/releases/gcc-%s' % br
+        get_revisions(repo, bname, old, seen, gittosvn, svntogit)
     return (gittosvn, svntogit)
 
-surgeon = parse_git('/tmp/gcc-reposurgeon-7b', 'origin/master', False)
+surgeon = parse_git('/tmp/gcc-reposurgeon-8', 'cutover-git', False)
 mirror = parse_git('/home/marxin/Programming/gcc', '4682b0a53b364ede1263a9e88951f3e81443113e', True)
 
-for git, svn in mirror[0].items():
-    if not svn in surgeon[1]:
-        print('Missing: %s' % svn)
+files = os.listdir('/home/marxin/DATA/gcc-binaries')
+existing = set([f.split('.')[0] for f in files])
+
+print(len(existing))
+have = 0
+for e in existing:
+    if e in mirror[0]:
+        have += 1
+
+print(have)
+print('3e7b85061947bdc7c7465743ba90734566860821' in mirror[0])
+print('e9c762ec4671d77e301492e4f9e92e1d3d667188' in mirror[0])
