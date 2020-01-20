9453de109f770bc195f04321f9246a185d22d609
Martin Liska <mliska@suse.cz>
2020-01-20 14:14:52 +0100

    Fix gcc-bisect script format.

--- gcc-bisect.py
+++ gcc-bisect.py
@@ -179,7 +179,7 @@ class GitRevision:
         hash = colored(self.short_hexsha(), description_color)
         if describe:
             r = subprocess.check_output('git gcc-descr --full %s' % self.commit.hexsha, cwd=git_location, shell=True, encoding='utf8')
-            hash += colored(r.strip(), 'green')
+            hash = colored(r.strip(), 'green')
         return '%s(%s)(%s)' % (hash, self.timestamp_str(), self.commit.author.email)
 
     def patch_name(self):
