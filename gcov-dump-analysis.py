d340fd5fe16e4eb60abd6e96f5837f67fb460204
Martin Liska <mliska@suse.cz>
2020-01-31 15:00:31 +0100

    Add Honza's changes.

--- gcov-dump-analysis.py
+++ gcov-dump-analysis.py
@@ -42,62 +42,117 @@ for needle in needles:
                         values = [int(x) for x in parts[1].strip().split(' ')]
                         buffer += values
 
+    allways_used_values = [0] * 5
+    allways_used_values_freq = [0] * 5
+    allways_used_values_hit = [0] * 5
+    all_used_values = [0] * 5
+    all_used_values_freq = [0] * 5
+    all_used_values_hit = [0] * 5
     used_values = [0] * (TOPN_COUNTERS + 1)
     used_values_freq = [0] * (TOPN_COUNTERS + 1)
-    all_invalid = []
-    invalid = 0
-    invalid_freq = 0
+    used_values_hit = [0] * (TOPN_COUNTERS + 1)
+    nonrep_used_values = [0] * 5
+    nonrep_used_values_freq = [0] * 5
+    nonrep_used_values_hit = [0] * 5
+    all_with_missing_vals = []
+    with_missing_vals = 0
+    with_missing_vals_freq = 0
     not_executed = 0
     one = 0
     one_freq = 0
     sum = 0
+    allways_sum = 0
+    all_sum = 0
+    rep_sum = 0
+    nonrep_sum = 0
 
     i = 0
     c = len(all) / TOPN_COUNTER_VALUES
     while i < c:
         topn = all[TOPN_COUNTER_VALUES * i: TOPN_COUNTER_VALUES * (i + 1)]
-        sum += topn[0]
+        sum += abs (topn[0])
         if topn[0] == 0:
             not_executed += 1
             i += 1
             continue
-        if topn[2] == -1 or topn[2] == -9223372036854775808:
-            invalid += 1
-            invalid_freq += topn[0]
-            all_invalid.append(topn)
+        if topn[0] < 0:
+            with_missing_vals += 1
+            with_missing_vals_freq += abs (topn[0])
+            all_with_missing_vals.append(topn)
+        match = False
+        for j in range(TOPN_COUNTERS):
+            if topn[2 * j + 2] == topn[0] * TOPN_COUNTERS:
+                match = True
+        if match and topn[0] != 0:
+            if topn[0] < 0:
+                print (error)
+            one += 1
+            one_freq += topn[0]
         else:
-            match = False
+            allways_used = 0
+            all_used = 0
+            used = 0
+            nonrep_used = 0
+            allways_hit = 0
+            all_hit = 0
+            hit = 0
+            nonrep_hit = 0
             for j in range(TOPN_COUNTERS):
-                if topn[2 * j + 2] == topn[0] * TOPN_COUNTERS:
-                    match = True
-            if match and topn[0] != 0:
-                one += 1
-                one_freq += topn[0]
-            else:
-                used = 0
-                for j in range(TOPN_COUNTERS):
-                    if topn[2 * j + 2] > topn[0] / 2:
-                        used += 1
-                used_values[used] += 1
-                used_values_freq[used] += topn[0]
+                if topn[2 * j + 2] > abs (topn[0]) / 2:
+                    allways_used += 1
+                    allways_sum += abs (topn[2 * j + 2])
+                    allways_hit += abs (topn[2 * j + 2])
+                if topn[0] > 0 and abs (topn[2 * j + 2]) > abs (topn[0]) / 2:
+                    all_used += 1
+                    all_sum += abs (topn[2 * j + 2])
+                    all_hit += abs (topn[2 * j + 2])
+                if abs (topn[2 * j + 2]) > abs (topn[0]) / 2:
+                    nonrep_used += 1
+                    nonrep_sum += abs (topn[2 * j + 2])
+                    nonrep_hit += abs (topn[2 * j + 2])
+                if (topn[2 * j + 2] > 0 or topn[0] > 0) and abs (topn[2 * j + 2]) > abs (topn[0]) / 2:
+                    used += 1
+                    rep_sum += abs (topn[2 * j + 2])
+                    hit += abs (topn[2 * j + 2])
+            allways_used_values[allways_used] += 1
+            allways_used_values_freq[allways_used] += abs (topn[0])
+            allways_used_values_hit[allways_used] += allways_hit
+            all_used_values[all_used] += 1
+            all_used_values_freq[all_used] += abs (topn[0])
+            all_used_values_hit[all_used] += all_hit
+            used_values[used] += 1
+            used_values_freq[used] += abs (topn[0])
+            used_values_hit[used] += hit
+            nonrep_used_values[nonrep_used] += 1
+            nonrep_used_values_freq[nonrep_used] += abs (topn[0])
+            nonrep_used_values_hit[nonrep_used] += nonrep_hit
 
         i += 1
 
     print('stats for %s:' % needle)
     print('  total: %d freq: %d' % (len(all) / TOPN_COUNTER_VALUES, sum))
     print('  not executed at all: %d' % (not_executed))
-    print('  invalid: %d (%2.2f%%) freq:%d (%2.2f%%)' % (invalid, 100 * invalid / c, invalid_freq, 100 * invalid_freq / sum))
+    print('  with missing vals: %d (%2.2f%%) freq:%d (%2.2f%%)' % (with_missing_vals, 100 * with_missing_vals / c, with_missing_vals_freq, 100 * with_missing_vals_freq / sum))
     print('  only one target: %d (%2.2f%%) freq:%d (%2.2f%%)' % (one, 100 * one / c, one_freq, 100 * one_freq / sum))
     print('  useful values (with not one target):')
     for i in range(TOPN_COUNTERS + 1):
-        print('    %d values: %8d times (%2.2f%%) freq:%12d (%2.2f%%)' % (i, used_values[i], 100.0 * used_values[i] / c, used_values_freq[i], 100 * used_values_freq[i] / sum))
+        print('    %d allways winning values: %8d times (%2.2f%%) freq:%12d (%2.2f%%) hitrate: (%2.2f%%)' % (i, allways_used_values[i], 100.0 * allways_used_values[i] / c, allways_used_values_freq[i], 100 * allways_used_values_freq[i] / sum, allways_used_values_hit[i]*100/allways_used_values_freq[i]/TOPN_COUNTERS))
+    print('  total hitrate in all runs: freq:%d (%2.2f%%)' % (one_freq + allways_sum/TOPN_COUNTERS, 100 * (one_freq + allways_sum/TOPN_COUNTERS) / sum))
+    for i in range(TOPN_COUNTERS + 1):
+        print('    %d all reproducible values: %8d times (%2.2f%%) freq:%12d (%2.2f%%) hitrate: (%2.2f%%)' % (i, all_used_values[i], 100.0 * all_used_values[i] / c, all_used_values_freq[i], 100 * all_used_values_freq[i] / sum, all_used_values_hit[i]*100/all_used_values_freq[i]/TOPN_COUNTERS))
+    print('  total hitrate with all counters valid: freq:%d (%2.2f%%)' % (one_freq + all_sum/TOPN_COUNTERS, 100 * (one_freq + all_sum/TOPN_COUNTERS) / sum))
+    for i in range(TOPN_COUNTERS + 1):
+        print('    %d some reroducible values: %8d times (%2.2f%%) freq:%12d (%2.2f%%) hitrate: (%2.2f%%)' % (i, used_values[i], 100.0 * used_values[i] / c, used_values_freq[i], 100 * used_values_freq[i] / sum, used_values_hit[i]*100/used_values_freq[i]/TOPN_COUNTERS))
+    print('  total hitrate with some counters valid: freq:%d (%2.2f%%)' % (one_freq + rep_sum/TOPN_COUNTERS, 100 * (one_freq + rep_sum/TOPN_COUNTERS) / sum))
+    for i in range(TOPN_COUNTERS + 1):
+        print('    %d non-reproducible values: %8d times (%2.2f%%) freq:%12d (%2.2f%%) hitrate: (%2.2f%%)' % (i, nonrep_used_values[i], 100.0 * nonrep_used_values[i] / c, nonrep_used_values_freq[i], 100 * nonrep_used_values_freq[i] / sum, nonrep_used_values_hit[i]*100/nonrep_used_values_freq[i]/TOPN_COUNTERS))
+    print('  total hitrate predicted non-reproducibly: freq:%d (%2.2f%%)' % (one_freq + nonrep_sum/TOPN_COUNTERS, 100 * (one_freq + nonrep_sum/TOPN_COUNTERS) / sum))
     print()
 
-    if invalid:
-        N = 10
-        print('Top %d invalid counters:' % N)
-        all_invalid = list(sorted(all_invalid, key = lambda x: x[0], reverse = True))
-        for i in range(N):
-            print('  freq: %.2f%%: %s' % (100 * all_invalid[i][0] / sum, all_invalid[i]))
-        print()
-        print()
+    N = 10
+    print('Top %d with missing vals counters:' % N)
+    all_with_missing_vals = list(sorted(all_with_missing_vals, key = lambda x: -x[0], reverse = True))
+    for i in range(N):
+        print('  freq: %.2f%%: %s' % (100 * all_with_missing_vals[i][0] / sum, all_with_missing_vals[i]))
+    print()
+    print()
