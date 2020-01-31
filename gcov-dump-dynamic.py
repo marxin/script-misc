e8c07fe417f369161dedc5e27032f11f6c7e3c3d
Martin Liska <mliska@suse.cz>
2020-01-31 15:51:25 +0100

    Add threshold as argument.

--- gcov-dump-dynamic.py
+++ gcov-dump-dynamic.py
@@ -10,6 +10,10 @@ needles = ['indirect_call', 'topn']
 
 threshold = 0.5
 
+if len(sys.argv) == 3:
+    threshold = float(sys.argv[2])
+
+print('Covered threshold: %.2f' % threshold)
 print('== Stats for %s ==' % location)
 for needle in needles:
     print('stats for %s:' % needle)
@@ -36,16 +40,19 @@ for needle in needles:
                             histogram[n] = [0, 0, 0]
                         histogram[n][0] += 1
                         if values:
-                            zipped = sorted(list(zip(values[::2], values[1::2])), key = lambda x: x[1], reverse = True)
-                            if zipped[0][1] >= (threshold * total):
-                                histogram[n][1] += 1
-                                histogram[n][2] += zipped[0][1]
-                                total_freq_with_half += zipped[0][1]
-                            elif n == 1:
-                                assert False
+                            zipped = sorted(list(zip(values[::2], values[1::2])), key = lambda x: x[1], reverse = True)[:8]
+                            for z in zipped:
+                                if z[1] >= (threshold * total):
+                                    histogram[n][1] += 1
+                                    histogram[n][2] += z[1]
+                                    total_freq_with_half += z[1]
+                                    if n == 256:
+                                        print('   ' + str(z))
+                                elif n == 1:
+                                    assert False
 
     print('Total: %d, total freq: %d, covered freq: %d (%.2f%%)' % (counter_count, total_freq, total_freq_with_half, 100.0 * total_freq_with_half / total_freq))
     print('Histogram:')
     for (k, v) in sorted(histogram.items(), key = lambda x: x[0]):
-        print('  %4d tracked: %5d (%.2f%%), >=0.5: %4d (cov. freq: %12d (%.2f%%))' % (k, v[0], 100.0 * v[0] / counter_count, v[1], v[2], 100.0 * v[2] / total_freq))
+        print('  %4d tracked: %5d (%.2f%%), >=%.2f: %4d (cov. freq: %12d (%.2f%%))' % (k, v[0], 100.0 * v[0] / counter_count, threshold, v[1], v[2], 100.0 * v[2] / total_freq))
     print()
