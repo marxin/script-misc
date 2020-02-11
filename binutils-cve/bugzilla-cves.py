04eb7ff3a716640a5ec4c5e55c49bbebc9ed6f1d
Martin Liska <mliska@suse.cz>
2020-02-11 16:36:02 +0100

    Use better binutils diff.

--- binutils-cve/bugzilla-cves.py
+++ binutils-cve/bugzilla-cves.py
@@ -3,7 +3,10 @@
 import xmltodict, json, re
 
 cve_regex = re.compile(r'CVE-[0-9]+-[0-9]+')
-bintuils_bugzilla_regex = re.compile(r'https:\/\/sourceware.org\/bugzilla\/show_bug\.cgi\?id=([0-9]+)')
+binutils_bugzilla_regex = re.compile(r'https:\/\/sourceware.org\/bugzilla\/show_bug\.cgi\?id=([0-9]+)')
+binutils_bugzilla_prefix = 'https://sourceware.org/bugzilla/show_bug.cgi?id='
+opensuse_bugzilla_prefix = 'https://bugzilla.opensuse.org/show_bug.cgi?id='
+pr_regex = re.compile(r'\+\s+PR ([\w-]+\/)?([0-9]+)')
 
 # the file is created from bugzilla with Export XML from all PRs assigned to matz@suse.com
 
@@ -11,9 +14,16 @@ data = xmltodict.parse(open('assigned.xml').read())
 bugs = data['bugzilla']['bug']
 
 # the file is created with:
-# find . -name ChangeLog | xargs -L1 git diff origin/binutils-2_33-branch..origin/binutils-2_34-branch >> diff && grep '+.*PR [0-9]' diff | sed 's/.*PR //' | sort -n | uniq
+# rm diff
+# find . -name ChangeLog | xargs -L1 git diff origin/binutils-2_33-branch..origin/binutils-2_34-branch >> diff
+# find . -name ChangeLog-[0-9]* | xargs -L1 git diff origin/binutils-2_33-branch..origin/binutils-2_34-branch >> diff
 
-mentioned_prs = set([int(l) for l in open('binutils-prs.txt').readlines()])
+mentioned_prs = set()
+for line in open('binutils.diff').readlines():
+    line = line.strip()
+    m = pr_regex.search(line)
+    if m:
+        mentioned_prs.add(int(m.group(2)))
 
 cve_bugs = []
 
@@ -26,7 +36,7 @@ for bug in bugs:
         comments = [bug['long_desc']]
     for comment in comments:
         text = comment['thetext']
-        for link in bintuils_bugzilla_regex.findall(text):
+        for link in binutils_bugzilla_regex.findall(text):
             links.add(int(link))
     links = list(links)
 
@@ -39,7 +49,9 @@ cve_bugs = list(sorted(cve_bugs, key = lambda x: x[0]))
 for cve in cve_bugs:
     print('%d (%s): %s' % (cve[0], cve[1], str(cve[2])))
 
-print('Mentioned CVEs in bintuils ChangeLogs:')
+print('\nMentioned CVEs in binutils ChangeLogs:')
 for cve in cve_bugs:
     if any(map(lambda x: x in mentioned_prs, cve[2])):
-        print('%d (%s): %s' % (cve[0], cve[1], str(cve[2])))
+        print('%d (%s): %s: %s%d' % (cve[0], cve[1], str(cve[2]), opensuse_bugzilla_prefix, cve[0]))
+        for b in cve[2]:
+            print('  %s%d' % (binutils_bugzilla_prefix, b))
