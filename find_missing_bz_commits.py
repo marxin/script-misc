7a17c0cc4f968dcb5d7c3887f842b08da472cf8f
Martin Liska <mliska@suse.cz>
2020-03-11 12:26:03 +0100

    Update script to download XML of PRs.

--- find_missing_bz_commits.py
+++ find_missing_bz_commits.py
@@ -5,6 +5,9 @@ import json
 import argparse
 import subprocess
 import re
+import sys
+
+import xmltodict
 
 base_url = 'https://gcc.gnu.org/bugzilla/rest.cgi/'
 pr_regex = re.compile('PR\ (.*)\/([0-9]+)')
@@ -15,34 +18,40 @@ def find_prs(message):
         if m:
             yield m.group(2)
 
-branches = ['origin/releases/gcc-8', 'origin/releases/gcc-9', 'origin/master']
+def get_commits_from_comments(pr):
+    sys.stderr.write('... get ' + pr + '\n')
+    r = requests.get('https://gcc.gnu.org/bugzilla/show_bug.cgi?id=%s&ctype=xml' % pr)
+    data = xmltodict.parse(r.text)
+    comments = data['bugzilla']['bug']['long_desc']
+    if type(comments) == list:
+        for comment in comments:
+            text = comment['thetext']
+            for l in text.split('\n'):
+                l = l.rstrip()
+                if l.startswith('commit r'):
+                    yield l
 
 commits = {}
-
-subprocess.check_output('wget https://gcc.gnu.org/pipermail/gcc-bugs/2020-March.txt.gz -O /tmp/march.txt.gz', shell = True)
-subprocess.check_output('gunzip -f /tmp/march.txt.gz', shell = True)
-
-bugzilla_messages = open('/tmp/march.txt').readlines()
-sent_comments = set()
-
-for l in bugzilla_messages:
-    l = l.rstrip()
-    if l.startswith('commit r'):
-        sent_comments.add(l)
+branches = ['origin/releases/gcc-8', 'origin/releases/gcc-9', 'origin/master']
 
 for b in branches:
     r = subprocess.check_output('git log %s --since=2020-03-04 --oneline --pretty=tformat:"%%H"' % b, shell = True, encoding = 'utf')
     for hash in r.strip().split('\n'):
         commits[hash] = subprocess.check_output('git log --format=medium -n 1 ' + hash, shell = True, encoding = 'utf8').strip()
 
-for hash, message in commits.items():
+items = commits.items()
+counter = 0
+for hash, message in items:
+    counter += 1
+    sys.stderr.write('%d/%d\n' % (counter, len(items)))
     prs = list(sorted(list(set(find_prs(message)))))
     if prs:
         for pr in prs:
             descr = subprocess.check_output('git gcc-descr --full ' + hash, shell = True, encoding = 'utf8').strip()
             needle = 'commit ' + descr
             needle2 = 'commit ' + hash
-            if not needle in sent_comments and not needle2 in sent_comments:
+            comment_commits = set(get_commits_from_comments(pr))
+            if not needle in comment_commits and not needle2 in comment_commits:
                 print('https://gcc.gnu.org/bugzilla/show_bug.cgi?id=%s' % pr)
                 message_lines = message.split('\n')
                 message_lines[0] = needle
