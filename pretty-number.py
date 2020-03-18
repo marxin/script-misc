e4bb762c7304e6a1fe15acdaa9f62c9a2dcfe5a5
Martin Liska <mliska@suse.cz>
2020-03-18 12:56:19 +0100

    Add pretty-number script.

---
+++ pretty-number.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import sys
+from textwrap import wrap
+
+def pad_string(s, n, prefix = ''):
+    if len(s) % n:
+        s = '0' * (n - len(s) % n) + s
+    return ' '.join(['%8s' % (prefix + x) for x in wrap(s, n)])
+
+number = int(eval(sys.argv[1]))
+
+print('%-16s: %d' % ('Integer', number))
+
+hexadecimal = hex(abs(number))
+assert hexadecimal.startswith('0x')
+hexadecimal = hexadecimal[2:]
+print('%-16s: %s' % ('Hexadecimal', pad_string(hexadecimal, 2, '0x')))
+if len(hexadecimal) % 2:
+    hexadecimal = '0' + hexadecimal
+print('%-16s: ' % 'Integer by byte', end = '')
+for b in wrap(hexadecimal, 2):
+    print('%8s ' % str(int(b, 16)), end = '')
+print()
+
+binary = bin(abs(number))
+assert binary.startswith('0b')
+binary = binary[2:]
+print('%-16s: %s' % ('Binary', pad_string(binary, 8)))
+
+print('%-16s: ' % 'Bits set', end = '')
+for i, v in enumerate(reversed(binary)):
+    if v == '1':
+        print('%d ' % i, end = '')
+print()
