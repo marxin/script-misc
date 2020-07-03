5efd3eb80e4fa4b957a046c29c4112659ee60996
Martin Liska <mliska@suse.cz>
2020-07-03 16:31:16 +0200

    Simplify finding of ld/ld.gold.

--- usage-wrapper.py
+++ usage-wrapper.py
@@ -51,8 +51,7 @@ def to_gigabyte(value):
 def get_process_name(proc):
     name = proc.name()
     cmdline = proc.cmdline()
-    if (name == 'ld' or name == 'ld.gold'
-            and len(cmdline) >= 2 and cmdline[1] == '-plugin'):
+    if name == 'ld' or name == 'ld.gold' and '-plugin' in cmdline:
         return 'ld'
     elif name == 'lto1-wpa':
         return 'WPA'
