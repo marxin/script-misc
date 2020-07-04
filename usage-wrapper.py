1b357f38db4d25410306f3b0980cf9e1bf442b6f
Martin Liska <mliska@suse.cz>
2020-07-04 15:26:15 +0200

    Fix y steps.

--- usage-wrapper.py
+++ usage-wrapper.py
@@ -166,8 +166,8 @@ def generate_graph(time_range):
     limit = 1
     while 1.2 * peak_memory > limit:
         limit *= 2
-    mem_subplot.set_ylim([0, limit])
-    mem_subplot.set_yticks(range(0, limit + 1, math.ceil((limit + 1) / 10)))
+    mem_subplot.set_ylim([0, limit + 1])
+    mem_subplot.set_yticks(range(0, limit + 1, math.ceil(limit / 8)))
     mem_subplot.grid(True)
 
     # TODO: move to a function
