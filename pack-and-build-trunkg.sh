c59377c2d86335178f6086dcf35e81665755dfbd
Martin Liska <mliska@suse.cz>
2020-07-09 15:38:32 +0200

    Use automatically threads for xz (150s -> 23s).

--- pack-and-build-trunkg.sh
+++ pack-and-build-trunkg.sh
@@ -48,7 +48,7 @@ echo "[revision $rev]" > $pkg/gcc/REVISION
 git archive --format=tar --prefix=$pkg/ -o $packagedir/$pkg.tar $rev
 tar rvf $packagedir/$pkg.tar $pkg/gcc/REVISION
 mv $pkg /tmp/
-xz $packagedir/$pkg.tar
+xz -T0 $packagedir/$pkg.tar
 ls -l $packagedir/$pkg.tar.xz
 
 cd $packagedir
