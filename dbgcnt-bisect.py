c3b6ffbb0f39c2f0b1163d854e22e70de4bbacd8
Martin Liska <mliska@suse.cz>
2020-10-08 14:34:09 +0200

    Add verbose mode.

--- dbgcnt-bisect.py
+++ dbgcnt-bisect.py
@@ -10,6 +10,7 @@ parser.add_argument('run_command', help='Run command')
 parser.add_argument('dbg_cnt_name', help='Debug counter name')
 parser.add_argument('max_argument', type=int,
                     help='Maximum value of the debug counter')
+parser.add_argument('-v', '--verbose', action = 'store_true', help = 'Verbose output')
 
 args = parser.parse_args()
 
@@ -20,7 +21,12 @@ while boundaries[1] - boundaries[0] > 1:
     print('%d:[%d, %d]' % (middle, boundaries[0], boundaries[1]))
     cmd = args.gcc_command + ' -fdbg-cnt=%s:%d' % (args.dbg_cnt_name, middle)
     subprocess.check_output(cmd, shell=True, stderr=subprocess.PIPE)
-    r = subprocess.run(args.run_command, shell=True, stderr=subprocess.PIPE)
+    r = subprocess.run(args.run_command, shell=True,
+                       stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                       encoding='utf8')
+    if args.verbose:
+        print(r.stdout)
+        print(r.stderr)
     if r.returncode == 0:
         boundaries[0] = middle
     else:
