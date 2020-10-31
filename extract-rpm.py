5137dfeaea33c36333ed241ef0a19af0b2f14479
Martin Liska <mliska@suse.cz>
2020-10-31 10:22:05 +0100

    Improve RPM extraction script.

--- extract-rpm.py
+++ extract-rpm.py
@@ -3,6 +3,7 @@
 import os
 import subprocess
 import argparse
+from pathlib import Path
 
 parser = argparse.ArgumentParser(description='Extract RPM file into a directory')
 parser.add_argument('rpm', metavar = 'rpm', help = 'RPM file locaction')
@@ -12,5 +13,5 @@ args = parser.parse_args()
 if not os.path.exists(args.folder):
     os.mkdir(args.folder)
 
-os.chdir(args.folder)
-subprocess.check_output('rpm2cpio %s | cpio -idmv' % args.rpm, shell = True)
+cmd_args = '-i' if '.src.rpm' in Path(args.rpm).name else '-idmv'
+subprocess.check_output('rpm2cpio %s | cpio %s -D %s' % (args.rpm, cmd_args, args.folder), shell = True)
