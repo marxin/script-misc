6f96363e6f0e5de431bcba38b1e8e84f773a6fc2
Martin Liska <mliska@suse.cz>
2020-11-23 14:33:35 +0100

    Refactor scipt a bit.

--- obs-error-parser.py
+++ obs-error-parser.py
@@ -5,6 +5,7 @@ import subprocess
 import shutil
 import os
 from termcolor import colored
+import concurrent.futures
 
 buffer = ''
 
@@ -21,39 +22,6 @@ def create_dir(path):
 tokens = ['error: ', 'internal compiler error: ', ' FAILED ']
 extra_lines = 1
 
-def grep_errors(log):
-    lines = log.split('\n')
-    for i, v in enumerate(lines):
-        if any(map(lambda x: x in v, tokens)):
-            v = ['  ' + x for x in lines[i - 1: i + 1]]
-            printme('\n'.join(v))
-
-def process_arch(url, folder, project, repository, arch):
-    create_dir(os.path.join(folder, repository))
-    arch_dir = os.path.join(folder, repository, arch)
-
-    result = subprocess.check_output('osc -A %s r %s -r %s -a %s --csv' % (url, project, repository, arch), shell = True)
-    packages = result.decode('utf-8', 'ignore').strip().split('\n')
-    packages = [x.split(';')[0] for x in packages if 'failed' in x or (args.all and 'succeeded' in x)]
-    packages = [x for x in packages if x != '_']
-
-    print('Packages: %d' % len(packages))
-
-    create_dir(arch_dir)
-    for index, package in enumerate(sorted(packages)):
-        for i in range(3):
-            try:
-                result = subprocess.check_output('osc -A %s remotebuildlog %s %s %s %s' % (url, project, package, repository, arch), shell = True)
-                log = result.decode('utf-8', 'ignore')
-                log_file = os.path.join(arch_dir, package + '.log')
-                with open(log_file, 'w+') as w:
-                    w.write(log)
-                printme('%d/%d: %s' % (index, len(packages), package))
-            except Exception as e:
-                print(e)
-
-            break
-
 parser = argparse.ArgumentParser(description = 'Download OBS log files for failed packages')
 parser.add_argument('url', help = 'OBS API url')
 parser.add_argument('folder', help = 'Destination folder')
@@ -65,9 +33,42 @@ args = parser.parse_args()
 
 shutil.rmtree(args.folder, ignore_errors = True)
 
+def grep_errors(log):
+    lines = log.split('\n')
+    for i, v in enumerate(lines):
+        if any(map(lambda x: x in v, tokens)):
+            v = ['  ' + x for x in lines[i - 1: i + 1]]
+            printme('\n'.join(v))
+
+def process_arch(arch):
+    create_dir(os.path.join(args.folder, args.repository))
+    arch_dir = os.path.join(args.folder, args.repository, arch)
+
+    result = subprocess.check_output('osc -A %s r %s -r %s -a %s --csv' % (args.url, args.project, args.repository, arch), shell = True)
+    packages = result.decode('utf-8', 'ignore').strip().split('\n')
+    packages = [x.split(';')[0] for x in packages if 'failed' in x or (args.all and 'succeeded' in x)]
+    packages = [x for x in packages if x != '_']
+
+    print('Packages: %d' % len(packages))
+
+    create_dir(arch_dir)
+    for index, package in enumerate(sorted(packages)):
+        for i in range(3):
+            try:
+                result = subprocess.check_output('osc -A %s remotebuildlog %s %s %s %s' % (args.url, args.project, package, args.repository, arch), shell = True)
+                log = result.decode('utf-8', 'ignore')
+                log_file = os.path.join(arch_dir, package + '.log')
+                with open(log_file, 'w+') as w:
+                    w.write(log)
+                printme('%d/%d: %s' % (index, len(packages), package))
+            except Exception as e:
+                print(e)
+
+            break
+
 for arch in args.archs:
     printme('== %s ==' % arch)
-    process_arch(args.url, args.folder, args.project, args.repository, arch)
+    process_arch(arch)
 
 with open(os.path.join(args.folder, 'build.log'), 'w+') as w:
     w.write(buffer)
