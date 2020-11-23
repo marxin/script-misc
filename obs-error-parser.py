1ecf30fe55ee8c2cefa09d216ba5595526b20326
Martin Liska <mliska@suse.cz>
2020-11-23 14:52:22 +0100

    Use threads in obs-error-parser.

--- obs-error-parser.py
+++ obs-error-parser.py
@@ -7,20 +7,12 @@ import os
 from termcolor import colored
 import concurrent.futures
 
-buffer = ''
-
-def printme(string):
-    global buffer
-    buffer += string + '\n'
-    print(string)
-
 def create_dir(path):
     if os.path.exists(path):
         return
     os.makedirs(path)
 
 tokens = ['error: ', 'internal compiler error: ', ' FAILED ']
-extra_lines = 1
 
 parser = argparse.ArgumentParser(description = 'Download OBS log files for failed packages')
 parser.add_argument('url', help = 'OBS API url')
@@ -33,12 +25,11 @@ args = parser.parse_args()
 
 shutil.rmtree(args.folder, ignore_errors = True)
 
-def grep_errors(log):
-    lines = log.split('\n')
-    for i, v in enumerate(lines):
-        if any(map(lambda x: x in v, tokens)):
-            v = ['  ' + x for x in lines[i - 1: i + 1]]
-            printme('\n'.join(v))
+def download_build_log(package, log_file):
+    global done
+    result = subprocess.check_output('osc -A %s remotebuildlog %s %s %s %s' % (args.url, args.project, package, args.repository, arch), shell = True, encoding='utf8')
+    with open(log_file, 'w+') as w:
+        w.write(result)
 
 def process_arch(arch):
     create_dir(os.path.join(args.folder, args.repository))
@@ -52,23 +43,15 @@ def process_arch(arch):
     print('Packages: %d' % len(packages))
 
     create_dir(arch_dir)
-    for index, package in enumerate(sorted(packages)):
-        for i in range(3):
-            try:
-                result = subprocess.check_output('osc -A %s remotebuildlog %s %s %s %s' % (args.url, args.project, package, args.repository, arch), shell = True)
-                log = result.decode('utf-8', 'ignore')
-                log_file = os.path.join(arch_dir, package + '.log')
-                with open(log_file, 'w+') as w:
-                    w.write(log)
-                printme('%d/%d: %s' % (index, len(packages), package))
-            except Exception as e:
-                print(e)
-
-            break
+    with concurrent.futures.ProcessPoolExecutor(max_workers=64) as executor:
+        futures = []
+        for package in packages:
+            log_file = os.path.join(arch_dir, package + '.log')
+            futures.append(executor.submit(download_build_log, package, log_file))
+        concurrent.futures.wait(futures)
+        for future in futures:
+            if future.exception():
+                print(future.exception())
 
 for arch in args.archs:
-    printme('== %s ==' % arch)
     process_arch(arch)
-
-with open(os.path.join(args.folder, 'build.log'), 'w+') as w:
-    w.write(buffer)
