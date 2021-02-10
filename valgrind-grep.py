ff0af3dc01f9101e368d2f67c735984e5fdcaa40
Martin Liska <mliska@suse.cz>
2021-02-10 13:19:32 +0100

    Add --exclude argument.

--- valgrind-grep.py
+++ valgrind-grep.py
@@ -6,17 +6,29 @@ import argparse
 def canonize_back_trace(bt):
     return tuple(line[line.find(':') + 1:].strip() for line in bt)
 
-def skip_bt(bt):
+
+def skip_bt(bt, exclude):
     for line in bt:
         if '/usr/bin/as' in line or 'collect2.c' in line:
             return True
+        for ex in exclude:
+            if ex in line:
+                return True
     return False
+
 
 bt_dict = {}
 
 parser = argparse.ArgumentParser(description='Group valgrind memory leaks')
 parser.add_argument('file', help='File with valgrind errors')
+parser.add_argument('--exclude', '-e',
+                    help='Exclude needles (separated by comma)')
 args = parser.parse_args()
+
+if args.exclude:
+    args.exclude = args.exclude.split(',')
+else:
+    args.exclude = []
 
 lines = [line.strip() for line in open(args.file).readlines()]
 for i in range(len(lines)):
@@ -28,7 +40,7 @@ for i in range(len(lines)):
             bt.append(lines[i])
             i += 1
         bt = canonize_back_trace(bt)
-        if not skip_bt(bt):
+        if not skip_bt(bt, args.exclude):
             bt_dict.setdefault(bt, 0)
             bt_dict[bt] += 1
 
