19b8ff03b0d37fb1f0134641c825704f416035f2
Martin Liska <mliska@suse.cz>
2021-09-15 14:43:57 +0200

    Simplify target list in batch build.

--- gcc-batch-target-build.py
+++ gcc-batch-target-build.py
@@ -5,83 +5,20 @@ import concurrent.futures
 import os
 import shutil
 import subprocess
+from itertools import dropwhile, takewhile
 
 
-default_targets = """aarch64-elf aarch64-linux-gnu aarch64-rtems
-  alpha-linux-gnu alpha-netbsd alpha-openbsd
-  alpha64-dec-vms alpha-dec-vms
-  amdgcn-amdhsa
-  arc-elf32OPT-with-cpu=arc600 arc-elf32OPT-with-cpu=arc700
-  arc-linux-uclibcOPT-with-cpu=arc700 arceb-linux-uclibcOPT-with-cpu=arc700
-  arm-netbsdelf
-  arm-linux-androideabi arm-uclinux_eabi arm-eabi arm-rtems
-  arm-symbianelf avr-elf
-  bfin-elf bfin-uclinux bfin-linux-uclibc bfin-rtems bfin-openbsd
-  bpf-unknown-none
-  c6x-elf c6x-uclinux cr16-elf cris-elf
-  csky-elf csky-linux-gnu
-  epiphany-elf epiphany-elfOPT-with-stack-offset=16 fido-elf
-  fr30-elf frv-elf frv-linux ft32-elf h8300-elf hppa-linux-gnu
-  hppa-linux-gnuOPT-enable-sjlj-exceptions=yes hppa64-linux-gnu
-  hppa2.0-hpux10.1 hppa64-hpux11.3
-  hppa64-hpux11.0OPT-enable-sjlj-exceptions=yes hppa2.0-hpux11.9
-  i686-pc-linux-gnu i686-apple-darwin i686-apple-darwin9 i686-apple-darwin10
-  i486-freebsd4 i686-freebsd6 i686-kfreebsd-gnu
-  i686-netbsdelf9
-  i686-openbsd i686-elf i686-kopensolaris-gnu i686-symbolics-gnu
-  i686-pc-msdosdjgpp i686-lynxos i686-nto-qnx
-  i686-rtems i686-solaris2.11 i686-wrs-vxworks
-  i686-wrs-vxworksae
-  i686-cygwinOPT-enable-threads=yes i686-mingw32crt ia64-elf
-  ia64-freebsd6 ia64-linux ia64-hpux ia64-hp-vms iq2000-elf lm32-elf
-  lm32-rtems lm32-uclinux m32c-rtems m32c-elf m32r-elf m32rle-elf
-  m32r-linux m32rle-linux m68k-elf m68k-netbsdelf
-  m68k-openbsd m68k-uclinux m68k-linux m68k-rtems
-  mcore-elf microblaze-linux microblaze-elf
-  mips-netbsd
-  mips64el-st-linux-gnu mips64octeon-linux mipsisa64r2-linux
-  mipsisa32r2-linux-gnu mipsisa64r2-sde-elf mipsisa32-elfoabi
-  mipsisa64-elfoabi mipsisa64r2el-elf mipsisa64sr71k-elf mipsisa64sb1-elf
-  mipsel-elf mips64-elf mips64vr-elf mips64orion-elf mips-rtems
-  mips-wrs-vxworks mipstx39-elf mmix-knuth-mmixware mn10300-elf moxie-elf
-  moxie-uclinux moxie-rtems
-  msp430-elf msp430-elfbare
-  nds32le-elf nds32be-elf
-  nios2-elf nios2-linux-gnu nios2-rtems
-  nvptx-none
-  or1k-elf or1k-linux-uclibc or1k-linux-musl or1k-rtems
-  pdp11-aout
-  powerpc-darwin8
-  powerpc-darwin7 powerpc64-darwin powerpc-freebsd6 powerpc-netbsd
-  powerpc-eabisimaltivec powerpc-eabisim ppc-elf
-  powerpc-eabialtivec powerpc-xilinx-eabi powerpc-eabi
-  powerpc-rtems
-  powerpc64-linux_altivec
-  powerpc-wrs-vxworks powerpc-wrs-vxworksae powerpc-wrs-vxworksmils
-  powerpc-lynxos powerpcle-elf
-  powerpcle-eabisim powerpcle-eabi
-  pru-elf
-  riscv32-unknown-linux-gnu riscv64-unknown-linux-gnu
-  powerpc-ibm-aix7.1 powerpc-ibm-aix7.2
-  rl78-elf rx-elf s390-linux-gnu s390x-linux-gnu s390x-ibm-tpf sh-elf
-  shle-linux sh-netbsdelf sh-superh-elf
-  sh-rtems sh-wrs-vxworks sparc-elf
-  sparc-leon-elf sparc-rtems sparc-linux-gnu
-  sparc-leon3-linux-gnuOPT-enable-target=all sparc-netbsdelf
-  sparc64-sun-solaris2.11OPT-with-gnu-ldOPT-with-gnu-asOPT-enable-threads=posix
-  sparc-wrs-vxworks sparc64-elf sparc64-rtems sparc64-linux sparc64-freebsd6
-  sparc64-netbsd sparc64-openbsd
-  tilegx-linux-gnuOPT-enable-obsolete tilegxbe-linux-gnuOPT-enable-obsolete
-  tilepro-linux-gnuOPT-enable-obsolete
-  v850e1-elf v850e-elf v850-elf v850-rtems vax-linux-gnu
-  vax-netbsdelf vax-openbsd visium-elf x86_64-apple-darwin
-  x86_64-pc-linux-gnuOPT-with-fpmath=avx
-  x86_64-elfOPT-with-fpmath=sse x86_64-freebsd6 x86_64-netbsd
-  x86_64-w64-mingw32
-  x86_64-mingw32OPT-enable-sjlj-exceptions=yes x86_64-rtems
-  xstormy16-elf xtensa-elf
-  xtensa-linux
-"""
+def parse_default_targets(gcc_root):
+    lines = open(os.path.join(gcc_root, 'contrib/config-list.mk')).read().splitlines()
+    lines = list(dropwhile(lambda x: not x.startswith('LIST ='), lines))
+    lines = list(takewhile(lambda x: x, lines))
+
+    targets = ' '.join(lines)
+    targets = targets.replace('\\', '')
+    for t in targets.split(' ')[2:]:
+        if t:
+            yield t
+
 
 JOBS = 4
 
@@ -92,7 +29,7 @@ parser.add_argument('-t', '--targets', help='Targets to be built (comma separate
 parser.add_argument('-p', '--preserve', action='store_true', help='Preserve built binaries')
 args = parser.parse_args()
 
-targets = [t.strip() for t in default_targets.split(' ') if t]
+targets = list(parse_default_targets(args.repository))
 if args.targets:
     targets = args.targets.split(',')
 
