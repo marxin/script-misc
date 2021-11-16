71440031cc0fdeaf19d63dfab65abc76a490b5b3
Martin Liska <mliska@suse.cz>
2021-11-16 11:22:34 +0100

    Add new script.

---
+++ bisect-object-files.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import shutil
+import sys
+
+filename = 'FILES.txt'
+
+parser = argparse.ArgumentParser(description='Drive bisection of object files.')
+parser.add_argument('good_dir', help='Directory with good object files')
+parser.add_argument('bad_dir', help='Directory with bad object files')
+parser.add_argument('work_dir', help='Working directory where to copy files')
+args = parser.parse_args()
+
+if not os.path.exists(filename):
+    print(f'File not found: {filename}')
+    sys.exit(1)
+
+
+# copy good files first
+good_total = 0
+for root, _, files in os.walk(args.good_dir):
+    for file in files:
+        if file.endswith('.o'):
+            subpath = os.path.relpath(os.path.join(root, file), args.good_dir)
+            shutil.copyfile(os.path.join(root, file), os.path.join(args.work_dir, subpath))
+            good_total += 1
+
+# then copy bad files
+files = open(filename).read().splitlines()
+for file in files:
+    shutil.copyfile(os.path.join(args.bad_dir, file), os.path.join(args.work_dir, file))
+
+print(f'Copied {good_total} good files and {len(files)} bad files')
