2e34ee5303323bb8b33da9dd13daf381e6636445
Martin Liska <mliska@suse.cz>
2021-12-06 11:36:25 +0100

    Improve script.

--- bisect-object-files.py
+++ bisect-object-files.py
@@ -18,18 +18,19 @@ if not os.path.exists(filename):
     sys.exit(1)
 
 
+bad_files = open(filename).read().splitlines()
+
 # copy good files first
 good_total = 0
 for root, _, files in os.walk(args.good_dir):
     for file in files:
-        if file.endswith('.o'):
+        if file not in bad_files and file.endswith('.o'):
             subpath = os.path.relpath(os.path.join(root, file), args.good_dir)
             shutil.copyfile(os.path.join(root, file), os.path.join(args.work_dir, subpath))
             good_total += 1
 
 # then copy bad files
-files = open(filename).read().splitlines()
-for file in files:
+for file in bad_files:
     shutil.copyfile(os.path.join(args.bad_dir, file), os.path.join(args.work_dir, file))
 
-print(f'Copied {good_total} good files and {len(files)} bad files')
+print(f'Copied {good_total} good files and {len(bad_files)} bad files')
