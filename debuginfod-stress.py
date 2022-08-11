136930edb2b4f7b55d3c3eb1cbfe7fda14ae8033
Martin Liska <mliska@suse.cz>
2022-08-11 18:11:05 +0200

    Add local reproducer.

--- debuginfod-stress.py
+++ debuginfod-stress.py
@@ -9,7 +9,7 @@ import requests
 ATTEMPTS = 10
 
 
-def get_debuginfo(binary, buildid):
+def get_debuginfo(binary, buildid, verbose):
     url = f'{args.URL}/buildid/{buildid}/debuginfo'
 
     for i in range(ATTEMPTS):
@@ -18,6 +18,8 @@ def get_debuginfo(binary, buildid):
             print(f'Attemp #{i}', binary, buildid, response)
             time.sleep(1)
         if response.status_code == 200:
+            if verbose:
+                print('.', end='', flush=True)
             return
 
 
@@ -25,6 +27,7 @@ parser = argparse.ArgumentParser(description='debuginfod stress test')
 parser.add_argument('FILE', help='Input file with checked binaries')
 parser.add_argument('URL', help='Server url')
 parser.add_argument('--workers', type=int, default=8, help='Number of threads')
+parser.add_argument('--verbose', '-v', action='store_true', help='Verbose')
 args = parser.parse_args()
 
 lines = open(args.FILE).read().strip().splitlines()
@@ -33,7 +36,7 @@ print(f'Running stress test of {len(lines)} binaries in {args.workers} threads:'
 with concurrent.futures.ProcessPoolExecutor(max_workers=args.workers) as executor:
     futures = []
     for buildid, binary in [x.split() for x in lines]:
-        futures.append(executor.submit(get_debuginfo, binary, buildid))
+        futures.append(executor.submit(get_debuginfo, binary, buildid, args.verbose))
     concurrent.futures.wait(futures)
     for future in futures:
         future.result()
