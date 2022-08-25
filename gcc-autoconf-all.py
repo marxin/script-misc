087fbfe0de472bbec6b0a81817c0ce3de2e5069d
Martin Liska <mliska@suse.cz>
2022-08-25 11:38:12 +0200

    Add new script.

---
+++ gcc-autoconf-all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import os
+from pathlib import Path
+import subprocess
+
+ENV = 'AUTOCONF=autoconf-2.69 ACLOCAL=~/bin/automake-1.15.1/bin/aclocal  AUTOMAKE=~/bin/automake-1.15.1/bin/automake'
+
+config_folders = []
+
+for root, _, files in os.walk('.'):
+    for file in files:
+        if file == 'configure':
+            config_folders.append(Path(root).resolve())
+
+for folder in config_folders:
+    print(folder)
+    os.chdir(folder)
+    if 'AC_CONFIG_HEADERS' in open('configure.ac').read():
+        subprocess.check_output(f'{ENV} autoheader-2.69 -f', shell=True, encoding='utf8')
+    subprocess.check_output(f'{ENV} autoconf-2.69 -f', shell=True, encoding='utf8')
