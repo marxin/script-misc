c742d06de74e3652123a52837dc80d238d58c910
Martin Liska <mliska@suse.cz>
2022-09-07 13:03:57 +0200

    Add install deps.

---
+++ pts/install-deps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+zypper in -y phoronix-test-suite cmake libtool 7zip openssl-devel nasm libyuv-devel ninja \
+        git libliquid-devel libbrotli-devel mpich gcc-fortran gcc-c++ make libogg-devel \
+        python3-devel libcurl-devel boost-devel libboost*devel python3-pybind11 cargo \
+        cfitsio-devel nodejs openmpi-devel qt6-opengl-devel lapack-devel ncurses5-devel \
+        libpng16-devel meson libnsl-devel opencv-devel libnl3-devel libjpeg8-devel \
+        libtiff-devel libopus-devel libXinerama-devel fftw3-devel libsndfile-devel \
+        SDL2-devel yasm opusfile-devel mbedtls-devel sdl12_compat-devel libopusenc-devel \
+        lksctp-tools-devel libxml2-devel hwloc-devel libXcursor-devel portmidi-devel \
+        portaudio-devel libuv-devel libXaw-devel
