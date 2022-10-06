96953edd1ef127c916e4e9ba36284547ce6236fa
Martin Liska <mliska@suse.cz>
2022-10-06 15:01:07 +0200

    Simplify elfshaker.

--- elfshaker.py
+++ elfshaker.py
@@ -1,50 +1,41 @@
 #!/usr/bin/env python3
 
+import concurrent.futures
 import os
 import shutil
 import subprocess
+import tempfile
 import time
 from pathlib import Path
 
 from git import Repo
 
 
-def wipe_loose():
-    shutil.rmtree('elfshaker_data/loose', ignore_errors=True)
-    shutil.rmtree('elfshaker_data/packs/loose', ignore_errors=True)
-
-
 CHUNK_SIZE = 100
+COMPRESSION_LEVEL = 17
 last_revision = '58a41b43b5f02c67544569c508424efa4115ad9f'
 repo = Repo('/home/marxin/Programming/gcc2')
-elfshaker_repo = '/home/marxin/elfshaker-gcc-binaries'
+elfshaker_repo = Path('/home/marxin/elfshaker-gcc-binaries')
+elfshaker_packs = elfshaker_repo / 'elfshaker_data' / 'packs'
 
 revisions = [x.hexsha for x in reversed(list(repo.iter_commits(last_revision + '..origin/master', first_parent=True)))]
 revcount = len(revisions)
 print(f'Have: {revcount} revisions')
 
 shutil.rmtree(elfshaker_repo, ignore_errors=True)
-os.mkdir(elfshaker_repo)
-os.chdir(elfshaker_repo)
+elfshaker_packs.mkdir(parents=True)
 
-for i in range(revcount // CHUNK_SIZE):
-    wipe_loose()
-    print(f'{i + 1}/{revcount // CHUNK_SIZE}')
-    if os.path.exists('elfshaker_data'):
-        mbsize = int(subprocess.check_output('du -s elfshaker_data', shell=True, encoding='utf8').split()[0])
-        mbsize /= 1024
-        print(f'Size {mbsize / (i * CHUNK_SIZE):.2f} MiB per revision')
-    chunk = revisions[:CHUNK_SIZE]
-    print('  ' + '.' * CHUNK_SIZE)
-    print('  ', end='')
 
+def pack_revisions(n, revisions):
     start = time.monotonic()
-    for h in chunk:
+    tempdir = tempfile.mkdtemp()
+    os.chdir(tempdir)
+    print(f'Packing {n} in {tempdir}')
+    for h in revisions:
         archive = f'{h}.tar'
         zstd_archive = f'{archive}.zst'
         abs_zstd_archive = f'/home/marxin/DATA/gcc-binaries/{zstd_archive}'
         if Path(abs_zstd_archive).exists():
-            print('#', end='', flush=True)
             shutil.copyfile(abs_zstd_archive, zstd_archive)
             subprocess.check_output(f'zstd -T0 -d {zstd_archive} -f', shell=True, stderr=subprocess.DEVNULL)
             subprocess.check_output(f'tar xvf {archive}', shell=True)
@@ -52,15 +43,21 @@ for i in range(revcount // CHUNK_SIZE):
             os.remove(zstd_archive)
             subprocess.check_output(f'/home/marxin/Programming/elfshaker/target/release/elfshaker store {h}',
                                     shell=True)
-        else:
-            print('.', end='', flush=True)
-    print()
-    duration = time.monotonic() - start
-    print(f'Store took {duration / CHUNK_SIZE:.2f}s per revision')
-
-    start = time.monotonic()
-    subprocess.check_output(f'~/Programming/elfshaker/target/release/elfshaker pack pack-{i} --compression-level 19',
+    subprocess.check_output(f'~/Programming/elfshaker/target/release/elfshaker pack pack-{i}'
+                            '--compression-level {COMPRESSION_LEVEL}',
                             shell=True, stderr=subprocess.PIPE)
-    revisions = revisions[CHUNK_SIZE:]
-    duration = time.monotonic() - start
-    print(f'Pack took {duration / CHUNK_SIZE:.2f}s per revision')
+    shutil.copy(f'elfshaker_data/packs/pack-{n}.pack', elfshaker_packs)
+    shutil.copy(f'elfshaker_data/packs/pack-{n}.pack.idx', elfshaker_packs)
+    shutil.rmtree(tempdir)
+    print(f'Packing {n} took {time.monotonic() - start:.2f}')
+
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+    futures = []
+    for i in range(revcount // CHUNK_SIZE):
+        chunk = revisions[:CHUNK_SIZE]
+        futures.append(executor.submit(pack_revisions, i, chunk))
+        revisions = revisions[CHUNK_SIZE:]
+    concurrent.futures.wait(futures)
+    for future in futures:
+        future.result()
