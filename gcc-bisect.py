737a9d76a6e11bbbee8691fab0f8a437c5b55b71
Martin Liska <mliska@suse.cz>
2023-02-13 10:35:07 +0100

    Add extra cleanup after packaging happens.

--- gcc-bisect.py
+++ gcc-bisect.py
@@ -579,6 +579,7 @@ class GitRepository:
             if len(topack) >= CHUNK_SIZE:
                 self.pack(packname, topack[:CHUNK_SIZE])
                 print(f'New pack {packname} created')
+                self.cleanup()
             else:
                 print(f'Loose objects waiting for packing: {len(topack)}')
 
